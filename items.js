46eab5afe629af8dced3cae21a1246630e5ebbd7
Shagan742 <526ssangha@frhsd.com>
2025-06-13 08:58:14 -0400

    product page
    Co-authored-by: LGreen808 <LGreen808@users.noreply.github.com>

---
+++ items.js
@@ -0,0 +1,33 @@
+const items= [
+    {
+        productName: 'Wooden Airplane',
+        photo:'imgs/plane1.jpg',
+        hiddenFact:'...'
+    },
+
+    {
+        productName:'Wooden Train Set',
+        photo:'imgs/train1.jpg',
+        hiddenFact:'... '
+    },
+
+    {
+        productName:'Wooden Boat',
+        photo:'imgs/boat1.jpg',
+        hiddenFact:'...'
+    },
+
+    {
+        productName:'Wooden Block Set',
+        photo:'imgs/block4 .jpg',
+        hiddenFact:'...'
+    },
+
+    {
+        productName:'Wooden Car',
+        photo:'imgs/car1.jpg',
+        hiddenFact:'...'
+    },
+
+    
+]
---
+++ script.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+    const grid=document.getElementById('rosterGrid')
+    const render= list => {
+        grid.innerHTML=''
+        list.forEach((item, i) => {
+            const col = document.createElement('div')
+            col.className='col-md-4 col-sm-12 d-flex justify-content-center my-5'
+            col.innerHTML=`
+            <div class="card h-100" style="width:210px;">
+                    <img src="${item.photo}" alt="${item.productName}" class="card-img-top">
+                    <div class="card-body text-center">
+                        <h5 class="card-title mb-4">${item.productName}</h5>
+
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal${i}">
+  More Info
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal${i}" tabindex="-1" aria-labelledby="exampleModalLabel${i}" aria-hidden="true">
+  <div class="modal-dialog" >
+    <div class="modal-content" style="background-color:black; border:2px solid white;">
+
+    <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel${i}">${item.productName}</h1>
+      </div>
+
+      <div class="modal-body">
+        <p class="hiddenFact">${item.hiddenFact}</p>   
+       <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+                    </div>
+                </div>
+            `
+
+            grid.appendChild(col)
+
+            
+        })
+
+
+    }
+
+
+
+
+render(items)
+})
